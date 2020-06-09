1d3babe7d1cef9d1f9f9cd59a8591f915b26c9e0
Shubham Gandhi <62895254+gandhi-shubh02@users.noreply.github.com>
2020-06-09 12:57:52 +0300

    Update Image Identifier.py
    Added extra layers for improved accuracy

--- Image Identifier.py
+++ Image Identifier.py
@@ -2,42 +2,48 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
 import math
+
 tfds.disable_progress_bar()
-                                                        # importing the fashion MNIST dataset provided by tensorflow
-dataset,metadata=tfds.load('fashion_mnist',as_supervised=True,with_info=True)
-features_train,features_test=dataset['train'],dataset['test']
-                                                        #60k images in features_train
-                                                         #10k images in features_test
-fashion_types=['T-shit/Top','Trouser','Pullover','Dress','Coat','Sandal','Shirt','Sneaker','Bag','Ankle boot']
+# importing the fashion MNIST dataset provided by tensorflow
+dataset, metadata = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
+features_train, features_test = dataset['train'], dataset['test']
+# 60k images in features_train
+# 10k images in features_test
+fashion_types = ['T-shit/Top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag',
+                 'Ankle boot']
 # Each image is one of the following fashion types
-num_of_train=metadata.splits['train'].num_examples
-num_of_test=metadata.splits['test'].num_examples
-def normalize(images,labels):                           #value of each pixel is from 0 to 255
-                                                        #function makes it to 0 or 1
-    images=tf.cast(images,tf.float32)                   # cast() converts the datatype of the dataset
-    images=images/255
-    return images,labels                        
+num_of_train = metadata.splits['train'].num_examples
+num_of_test = metadata.splits['test'].num_examples
 
 
-                                                   
-features_train=features_train.map(normalize)            #map itrates all images in the dataset
-features_test=features_train.map(normalize)             #cache() keeps data in ram to make trainning faster
+def normalize(images, labels):  # value of each pixel is from 0 to 255
+    # function makes it to 0 or 1
+    images = tf.cast(images, tf.float32)  # cast() converts the datatype of the dataset
+    images = images / 255
+    return images, labels
 
-l0=tf.keras.layers.Flatten(input_shape=(28,28,1))       # converts a 2d image(28x28 pixels) into a 1d array of 784 pixels
-l1=tf.keras.layers.Dense(128,activation=tf.nn.relu)
-l2=tf.keras.layers.Dense(10,activation=tf.nn.softmax)
-model=tf.keras.Sequential([l0,l1,l2])                   #10 beacuse 10 probabilities of output
-model.compile(optimizer='adam',loss=tf.keras.losses.SparseCategoricalCrossentropy(),metrics=['accuracy'])
-                                                            
-features_train=features_train.cache().repeat().shuffle(num_of_train).batch(32)        #dataset.batch(32) tells model.fit to use batches of 32 images and labels when updating the model variables.
-features_test=features_test.cache().repeat().shuffle(num_of_test).batch(32)
-model.fit(features_train,epochs=5,steps_per_epoch=math.ceil(num_of_train/32))
-#softmax returns answer in probability
-test_loss,test_accuracy=model.evaluate(features_test,steps=math.ceil(num_of_test/32))
-print('test acc',test_accuracy)
-for test_images, test_labels in features_train:       #predictions
-  test_images = test_images.numpy()
-  test_labels = test_labels.numpy()
-  predictions = model.predict(test_images)
-for i in predictions.take(1):                           #printing the output of the highest probability ie  the type of clothing
-    print(np.argmax(i))
+
+features_train = features_train.map(normalize)  # map itrates all images in the dataset
+features_test = features_train.map(normalize)  # cache() keeps data in ram to make trainning faster
+l0=tf.keras.layers.Conv2D(32,(3,3),padding='same',activation=tf.nn.relu)
+l1=tf.keras.layers.MaxPooling2D((2,2),strides=2)
+l2=tf.keras.layers.Conv2D(64,(3,3),padding='same',activation=tf.nn.relu)
+l3=tf.keras.layers.MaxPooling2D((2,2),strides=2)
+l4 = tf.keras.layers.Flatten()  # converts a 2d image(28x28 pixels) into a 1d array
+l5 = tf.keras.layers.Dense(128, activation=tf.nn.relu)
+l6 = tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+model = tf.keras.Sequential([l0, l1, l2,l3,l4,l5,l6])  # 10 beacuse 10 probabilities of output
+model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(), metrics=['accuracy'])
+
+features_train = features_train.cache().repeat().shuffle(num_of_train).batch(
+    32)  # dataset.batch(32) tells model.fit to use batches of 32 images and labels when updating the model variables.
+features_test = features_test.cache().repeat().shuffle(num_of_test).batch(32)
+model.fit(features_train, epochs=10, steps_per_epoch=math.ceil(num_of_train / 32))
+# softmax returns answer in probability
+test_loss, test_accuracy = model.evaluate(features_test, steps=math.ceil(num_of_test / 32))
+print('test acc', test_accuracy)
+for test_images, test_labels in features_test.take(1):  # predictions
+    test_images = test_images.numpy()
+    test_labels = test_labels.numpy()
+    predictions = model.predict(test_images)
+print(predictions[0])
